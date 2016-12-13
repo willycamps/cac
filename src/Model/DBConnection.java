c7f30be214c08c13422fc6badef9f8fcf3949da3
wcampos <wcampos@estratek.com>
2016-12-12 23:36:22 -0600

    Add DB Connection

---
+++ src/Model/DBConnection.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Model;
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+/**
+ *
+ * @author W. Estuardo
+ */
+public class DBConnection 
+{
+public static Connection createConnection()
+ {
+    Connection con = null;
+    String url = "jdbc:mysql://localhost:3306/ravi"; 
+   //for SQL and oracle or any other db server this url differs. 
+    //where ravi is the database name and 3306 is the port where mysql is running
+    String username = "root"; //username of database user
+    String password = "root"; //password
+
+    try
+     {
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver");// differs from DB server to server
+        }
+        catch (ClassNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+
+       con = DriverManager.getConnection(url, username, password);
+    }
+    catch (Exception e)
+    {
+        e.printStackTrace();
+    }
+
+return con;
+ }
+    
+    
+}
